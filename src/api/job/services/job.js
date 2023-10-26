08b3fd2e3552edabc7f3c5ec5f6a60e39f3336b0
Pennymike <gbanyamichael1@gmail.com>
2023-10-27 07:40:57 +1100

    added services

--- src/api/job/services/job.js
+++ src/api/job/services/job.js
@@ -1,9 +1,43 @@
-'use strict';
+"use strict";
 
 /**
  * job service
  */
 
-const { createCoreService } = require('@strapi/strapi').factories;
+module.exports = ({ strapi }) => ({
+  async find(params) {
+    const { start = 0, limit = 10, ...rest } = params;
 
-module.exports = createCoreService('api::job.job');
+    try {
+      const [entries, totalCount] = await Promise.all([
+        strapi.entityService.findMany("api::job.job", {
+          start,
+          limit,
+          ...rest,
+        }),
+
+        strapi.entityService.count("api::job.job", params),
+      ]);
+
+      const totalPages = Math.ceil(totalCount / limit);
+      const currentPage = Math.min(totalPages, Math.max(1, start));
+      const hasPrevPage = currentPage > 1;
+      const hasNextPage = currentPage < totalPages;
+      return {
+        entries,
+        meta: {
+          paginate: {
+            totalCount,
+            totalPages,
+            currentPage,
+            hasPrevPage,
+            hasNextPage,
+          },
+        },
+      };
+    } catch (error) {
+      strapi.log.error(error);
+      throw error;
+    }
+  },
+});
